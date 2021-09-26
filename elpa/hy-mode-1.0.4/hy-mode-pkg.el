2eaffb8b050c81309ae10c318d3c977b7b87562a
Jay Mellor <jay@taoleadership.co.uk>
2021-09-26 18:47:16 +0100

    Added elpa files

---
+++ elpa/hy-mode-1.0.4/hy-mode-pkg.el
@@ -0,0 +1,11 @@
+(define-package "hy-mode" "1.0.4" "Major mode for Hylang"
+  '((dash "2.13.0")
+    (dash-functional "1.2.0")
+    (s "1.11.0")
+    (emacs "24"))
+  :commit "e2d5fecdaec602788aa7123ed13651c888b8d94b" :keywords
+  '("languages" "lisp" "python")
+  :url "http://github.com/hylang/hy-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
