8f26425c4360669381b56d510662a7674399840f
Jay Mellor <jay@taoleadership.co.uk>
2021-11-17 12:14:34 +0000

    Added Org mode and friends

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;; Initialise package sources
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
@@ -56,7 +56,8 @@
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :config
+  (setq doom-modeline-height 35))
 
 (use-package doom-themes
   :config
@@ -113,8 +114,9 @@
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :init
-  (when (file-directory-p "~/Documents/CulturBook/")
-    (setq projectile-project-search-path '("~/Documents/CulturBook/")))
+  ; remove below?
+;  (when (file-directory-p "~/Documents/CulturBook/")
+ ;   (setq projectile-project-search-path '("~/Documents/CulturBook/")))
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package counsel-projectile
@@ -124,7 +126,62 @@
   ;:custom
   ;; diff in same window
   ;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
-  ) 
+  )
+
+(use-package forge) ;; todo forge-pull
+
+(use-package org
+  :config
+  (setq org-ellipsis " ."
+	;; hide *, / etc
+	org-hide-emphasis-markers t)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-agenda-files '("~/Documents/Learning and Testing apps/emacs/Tasks.org"))
+
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+	  (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "|" "COMPLETED(c)" "CANCELLED(k@)")))
+
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
+;; org-agenda-custom-commands
+
+(defun my/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+		      (expand-file-name "~/.emacs.d/Emacs.org"))
+    (let ((org-confirm-babel-evaulate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'my/org-babel-tangle-config))) ; can this be done with require?
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+(use-package smartparens
+  :hook (prog-mode . smartparens-strict-mode)
+  :bind (("C-<right>" . sp-forward-slurp-sexp)
+	 ("C-<left>" . sp-forward-barf-sexp))
+  :config (require 'smartparens-config))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l") ; or C-l or s-l
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -132,7 +189,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit counsel-projectile general doom-themes helpful ivy-rich which-key which-keys rainbow-delimiters use-package doom-modeline counsel command-log-mode))
+   '(typescript-mode lsp-mode smartparens org-bullets forge magit counsel-projectile general doom-themes helpful ivy-rich which-key which-keys rainbow-delimiters use-package doom-modeline counsel command-log-mode))
  '(same-window-regexps nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
