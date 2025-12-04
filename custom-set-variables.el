95d07ff0c2f059025f4a0d2f21b0a796da3cf721
Jay Mellor <jaymellor25@gmail.com>
2025-12-04 12:50:43 +0000

    custom-set-variables update

--- custom-set-variables.el
+++ custom-set-variables.el
@@ -5,12 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(add-node-modules-path all-the-icons company counsel
-						   counsel-projectile dap-mode flycheck
+						   counsel-projectile dap-mode eat flycheck
 						   general go-mode helpful ivy-rich lsp-mode
-						   lsp-ui magit markdown-mode org-bullets
-						   prettier-js prettier-rc projectile
-						   rainbow-delimiters smartparens treesit
-						   typescript-mode web-mode which-key)))
+						   lsp-ui magit markdown-mode multiple-cursors
+						   org-bullets prettier-js prettier-rc
+						   projectile rainbow-delimiters smartparens
+						   treesit typescript-mode web-mode which-key))
+ '(same-window-regexps nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
